699f5d5e06e47780ed763cad6df1457fcfed643e
Igor Belogolovsky <igor@belogolovsky.net>
2021-04-16 13:50:04 +0300

    Typo

--- src/main/java/com/jm/students/model/ServiceRequestHistory.java
+++ src/main/java/com/jm/students/model/ServiceRequestHistory.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "service_reqiests_history")
+@Table(name = "service_requests_history")
 @NoArgsConstructor
 @Getter
 @Setter
