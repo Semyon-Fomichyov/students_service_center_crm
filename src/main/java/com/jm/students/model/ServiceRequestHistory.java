9a291fdeacafc15550e75f0f9b38762d38707213
Igor Belogolovsky <igor@belogolovsky.net>
2021-04-16 13:44:41 +0300

    Initial commit

---
+++ src/main/java/com/jm/students/model/ServiceRequestHistory.java
@@ -0,0 +1,30 @@
+package com.jm.students.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "service_reqiests_history")
+@NoArgsConstructor
+@Getter
+@Setter
+public class ServiceRequestHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE
+            , CascadeType.REFRESH, CascadeType.DETACH})
+    @JoinColumn("service_requests_id")
+    private ServiceRequest serviceRequest;
+
+    @Enumerated(EnumType.STRING)
+    private StatusRequestType statusRequestType;
+}
