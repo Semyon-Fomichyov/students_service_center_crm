db1260eb9e72f241f65c0665c9a3a33ae33f1256
Igor Belogolovsky <igor@belogolovsky.net>
2021-04-18 16:31:09 +0300

    Save events history when add or change service request

--- src/main/java/com/jm/students/service/ServiceRequestServiceImpl.java
+++ src/main/java/com/jm/students/service/ServiceRequestServiceImpl.java
@@ -1,12 +1,14 @@
 package com.jm.students.service;
 
-import com.jm.students.model.StatusRequestType;
 import com.jm.students.model.ServiceRequest;
+import com.jm.students.model.ServiceRequestEvent;
+import com.jm.students.model.StatusRequestType;
 import com.jm.students.repository.ServiceRequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -14,10 +16,13 @@ import java.util.List;
 public class ServiceRequestServiceImpl implements ServiceRequestService {
 
     private final ServiceRequestRepository requestRepository;
+    private final ServiceRequestEventService serviceRequestEventService;
 
     @Autowired
-    public ServiceRequestServiceImpl(ServiceRequestRepository requestRepository) {
+    public ServiceRequestServiceImpl(ServiceRequestRepository requestRepository,
+                                     ServiceRequestEventService serviceRequestEventService) {
         this.requestRepository = requestRepository;
+        this.serviceRequestEventService = serviceRequestEventService;
     }
 
     @Override
@@ -28,11 +33,21 @@ public class ServiceRequestServiceImpl implements ServiceRequestService {
     @Override
     public void saveServiceRequest(ServiceRequest request) {
         requestRepository.saveServiceRequest(request);
+        ServiceRequestEvent event = new ServiceRequestEvent();
+        event.setServiceRequest(request);
+        event.setStatusRequestType(request.getStatusRequestType());
+        event.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        serviceRequestEventService.newEvent(event);
     }
 
     @Override
     public void updateServiceRequest(ServiceRequest request) {
         requestRepository.updateServiceRequest(request);
+        ServiceRequestEvent event = new ServiceRequestEvent();
+        event.setServiceRequest(request);
+        event.setStatusRequestType(request.getStatusRequestType());
+        event.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        serviceRequestEventService.newEvent(event);
     }
 
     @Override
@@ -49,7 +64,8 @@ public class ServiceRequestServiceImpl implements ServiceRequestService {
      * Метод находит заявку по {@param id},
      * устанавливает этой заявке новый статус {@param statusRequestType}
      * и изменяет заявку в базе
-     * @param id идентификатор заявки
+     *
+     * @param id                идентификатор заявки
      * @param statusRequestType новый статус заявки
      */
     @Override
@@ -57,5 +73,10 @@ public class ServiceRequestServiceImpl implements ServiceRequestService {
         ServiceRequest serviceRequest = getServiceRequestById(id);
         serviceRequest.setStatusRequestType(statusRequestType);
         updateServiceRequest(serviceRequest);
+        ServiceRequestEvent event = new ServiceRequestEvent();
+        event.setServiceRequest(serviceRequest);
+        event.setStatusRequestType(statusRequestType);
+        event.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        serviceRequestEventService.newEvent(event);
     }
 }
