955c7445c03437a61dac2a6ad6d8bef566350623
Igor Belogolovsky <igor@belogolovsky.net>
2021-04-25 15:00:44 +0300

    Merged with current master

--- src/main/java/com/jm/students/service/ServiceRequestServiceImpl.java
+++ src/main/java/com/jm/students/service/ServiceRequestServiceImpl.java
@@ -1,5 +1,6 @@
 package com.jm.students.service;
 
+import com.jm.students.model.ServiceRequestEvent;
 import com.jm.students.model.StatusRequestType;
 import com.jm.students.model.ServiceRequest;
 import com.jm.students.repository.ServiceRequestRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -16,16 +18,20 @@ public class ServiceRequestServiceImpl extends AbstractEntityServiceImpl<Service
 
     private final ServiceRequestRepository requestRepository;
 
+    private final ServiceRequestEventService serviceRequestEventService;
+
     @Autowired
-    public ServiceRequestServiceImpl(ServiceRequestRepository requestRepository) {
+    public ServiceRequestServiceImpl(ServiceRequestRepository requestRepository, ServiceRequestEventService serviceRequestEventService) {
         super(requestRepository);
         this.requestRepository = requestRepository;
+        this.serviceRequestEventService = serviceRequestEventService;
     }
     /**
      * Метод находит заявку по {@param id},
      * устанавливает этой заявке новый статус {@param statusRequestType}
      * и изменяет заявку в базе
-     * @param id идентификатор заявки
+     *
+     * @param id                идентификатор заявки
      * @param statusRequestType новый статус заявки
      */
     @Override
@@ -33,5 +39,9 @@ public class ServiceRequestServiceImpl extends AbstractEntityServiceImpl<Service
         ServiceRequest serviceRequest = findById(id);
         serviceRequest.setStatusRequestType(statusRequestType);
         update(serviceRequest);
-    }
-}
+        ServiceRequestEvent event = new ServiceRequestEvent();
+        event.setServiceRequest(serviceRequest);
+        event.setStatusRequestType(statusRequestType);
+        event.setTimestamp(new Timestamp(System.currentTimeMillis()));
+        serviceRequestEventService.newEvent(event);
+    }}
